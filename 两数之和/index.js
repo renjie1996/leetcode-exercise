99d261100e4169f69864290b01490e54e55101e5
renjie1996 <boomerangerrr@gmail.com>
2019-09-18 15:39:01 +0800

    feat: 添加注释

--- 两数之和/index.js
+++ 两数之和/index.js
@@ -1,3 +1,10 @@
+/**
+ * 利用数字的相加特性做mapping，例如和为10
+ * 2 -> 8, 3 -> 7
+ * map中一一对应，时间O(n)即可解决，伴随空间复杂O(n)
+ * 
+ * 如果使用两次循环，最坏情况则是时间复杂O(n2)，空间复杂 O(1)
+ */
 const twoSum = (nums, target) => {
   const map = new Map()
   for(let i = 0; i < nums.length; i++) {
